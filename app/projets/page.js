5b10fc4d496febe2c7a2c7eba6463e1f095088c6
User <emmanuellejupon@gmail.com>
2025-02-07 18:23:37 +0100

    clean code and export

--- app/projets/page.js
+++ app/projets/page.js
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import Header from "../_components/Header.jsx";
 import Hero from "../_components/Hero.jsx";
 import Spacing from "../_components/Spacing.jsx";
 import Status from "../_components/Status.jsx";
